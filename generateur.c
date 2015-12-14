1f9a6018f5c03d8bc8221c9e8a1e410546538f7f
Joseph Moullart De Vilmarest <moullart@clipper.ens.fr>
2015-12-14 17:58:14 +0100

    modidife: generateur

--- generateur.c
+++ generateur.c
@@ -1,18 +1,18 @@
 // C'est mal écrit mais je ne connais presque pas le C !
-// J'ai 2 problèmes: d'abord je ne comprends pas très bien le type Graph et je n'arrive pas à donner graphe_aleatoire sous ce type.
-// L'autre problème est que ce programme renvoie toujours la même chose, cela vient surement de rand mais je ne sais pas pourquoi.
-// Je suppose que le problème sera réglé quand je génèrerai plusieurs fois de suite des graphes.
-
+// problème: je ne comprends pas très bien le type Graph et je n'arrive pas à donner graphe_aleatoire sous ce type.
 
 #include "graph.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 void graphe_aleatoire (int nbNoeuds) {
     int t[nbNoeuds][nbNoeuds];
     int i, j, nbAretes=0;
     int boolean, s = 0;
-
+    
+    srand(time(NULL));
+    
     for (i=0; i<nbNoeuds; i++) {
         for (j=0; j<i; j++) {
             boolean = rand() % 2;
@@ -24,7 +24,9 @@ void graphe_aleatoire (int nbNoeuds) {
         t[i][i] = 0;
     }
     
-    printf("%d %d\n", nbNoeuds, nbAretes); // ici au lieu de l'afficher il faudrait le traduire en un graphe de type Graph
+    printf("%d %d\n", nbNoeuds, nbAretes);
+    // ici au lieu de l'afficher il faudrait le traduire en un graphe de type Graph
+    // sinon je les mets dans des .txt et on les exécute manuellement
     for (i=0; i<nbNoeuds; i++) {
         for (j=0; j<nbNoeuds; j++)
             s = s + t[i][j];
